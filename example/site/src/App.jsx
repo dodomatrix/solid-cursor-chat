dd9054e7e63d181c1174d3f5a907658c3da1bee7
osdodo <2925007984@qq.com>
2022-02-14 15:28:52 +0800

    fix netlify deployments

--- example/site/src/App.jsx
+++ example/site/src/App.jsx
@@ -1,4 +1,5 @@
-import CursorChat from '../../../dist/esm';
+// import CursorChat from '../../../dist/esm';
+import CursorChat from 'solid-cursor-chat';
 import logo from './assets/logo.png';
 import avatar from './assets/avatar.png';
 
